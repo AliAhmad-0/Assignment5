0d1925d6d59b00f9de67e9f2fca749a6a183288f
AliAhmad-0 <aliahmad554327@gmail.com>
2024-09-19 00:02:58 +0500

    condition function

---
+++ class05.py
@@ -0,0 +1,93 @@
+# Write a program that takes the age of a person as input and determines whether
+# they are a child (0-12 years),teenager (13-19 years), 
+# adult (20-59 years), or senior citizen (60 years and above)
+
+# age = int(input("enter the age:"))
+# if age >= 0 and age <= 12:
+#     print("you are child")
+# elif age >= 13 and age <= 19:
+#     print("you are teenage")
+# elif age >= 20 and age <= 59:
+#     print("you are adult")
+# else:
+#     print("senior citizen")    
+       
+
+
+
+
+
+
+
+
+# prompt: Take the user age.
+#   If the age is 18 or above:
+#  Ask if they have a nationality of "Pakistani".
+#  If yes, print "You are eligible to vote.
+#  If no, print "Please obtain a valid ID to vote
+
+
+# age = int(input("enter your age:"))
+# if age >= 18:
+#     nationality = input("do you have pakistani nationality? (yes/no):")
+#     if nationality.lower() == "yes":
+#         print("you are eligible for vote.")
+#     else:
+#         print("please obtain a valid ID to vote")
+# else:
+#     print("you are not eligible to vote.")        
+
+
+
+
+# Using conditional statements, check if the number is:
+# Whether it is divisible by both 2 and 3 or 
+# anyone of them or not divisible by both check 
+# all the cases and print statement for each case.
+
+# number = int (input("enter the number:"))
+# if number % 2 == 0 or number % 3 == 0:
+#     print("number is visible by both 2 and 3")
+# elif number % 2 == 0: 
+#     print (" number is visible by 2.")
+# elif number % 3 == 0:
+#     print("number is visible by 3.")
+# else:
+#     print("number is not visible by both 2 and 3.")
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+# Using conditional statements, check if the number is:
+# Positive, Negative, or Zero.
+
+# number = int(input("enter the number:"))
+# if number > 0:
+#     print("number is positive.")
+# elif number < 0:
+#     print("number is Negative.")
+# else:
+#     print("number is zero.")        
+
+
+
+
+
+
+# Using conditional statements, check if the number is:
+#  - Even or Odd.
+
+# number = int(input("Enter a number:"))
+# if number % 2 == 0:
+#     print("number is even")
+# else:
+#     print("number is odd")
